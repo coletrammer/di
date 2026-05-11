efdff890f7717294309e05798d8d6941472fae6c
coletrammer <cole.trammer@gmail.com>
2026-05-10 23:00:52 -0700

    meta: support aarch64 (char is unsigned)

--- include/di/types/compare_outcome.h
+++ include/di/types/compare_outcome.h
@@ -2,5 +2,5 @@
 
 namespace di::types::detail {
 // NOTE: this is the same layout libstdc++ uses, so don't change this. The compiler might rely on it.
-enum class CompareOutcome : char { Less = -1, Equal = 0, Greater = 1, Unordered = 2 };
+enum class CompareOutcome : char { Less = char(-1), Equal = 0, Greater = 1, Unordered = 2 };
 }
